7095bda9fd9288e76162b7726e17c9ce5afd33ff
miguepollo <127879645+miguepollo@users.noreply.github.com>
2024-03-18 08:58:40 +0100

    Update lv7_helper.h

--- src/lv7_helper.h
+++ src/lv7_helper.h
@@ -7,6 +7,7 @@ void startLvglTick();
 void stopLvglTick();
 void lvgl_whirling(uint8_t rot);
 static void TOUCH_IRQ_HANDLE(void);
+static void disp_flush(lv_disp_drv_t *disp_drv, const lv_area_t *area, lv_color_t *color_p);
 void disableTouchIRQ();
 void enableTouchIRQ();
 private:
