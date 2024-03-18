6673e226648c31820be5a8161c5439804fe24fc1
miguepollo <127879645+miguepollo@users.noreply.github.com>
2024-03-18 08:57:06 +0100

    Update lv7_helper.h

--- src/lv7_helper.h
+++ src/lv7_helper.h
@@ -1,10 +1,12 @@
 #include "lvgl.h"
 #include "focaltech.h"
 
+public:
 bool lvgl_begin();
 void startLvglTick();
 void stopLvglTick();
 void lvgl_whirling(uint8_t rot);
 static void TOUCH_IRQ_HANDLE(void);
 void disableTouchIRQ();
-void enableTouchIRQ()
+void enableTouchIRQ();
+private:
