c4efa3f051ad816a44c4c276d74df77573797750
Miguepollo <miguelgarciapel@protonmail.com>
2024-03-26 10:19:20 +0100

    Update lv-7helper

--- src/lv7_helper.h
+++ src/lv7_helper.h
@@ -4,18 +4,18 @@
 #include "focaltech.h"
 #include <Arduino.h>
 
-class lvgl_helper
-{
-public:
-bool lvgl_begin();
-void startLvglTick();
-void stopLvglTick();
-void lvgl_whirling(uint8_t rot);
-static void TOUCH_IRQ_HANDLE(void);
-static void disp_flush(lv_disp_drv_t *disp_drv, const lv_area_t *area, lv_color_t *color_p);
-void disableTouchIRQ();
-void enableTouchIRQ();
-};
-private:
+class lvgl_7helperForS3{
 
-extern lvgl_helper lvgl;
+public:
+    bool lvgl_begin();
+    void startLvglTick();
+    void stopLvglTick();
+    void lvgl_whirling(uint8_t rot);
+    static void TOUCH_IRQ_HANDLE(void);
+    static void disp_flush(lv_disp_drv_t *disp_drv, const lv_area_t *area, lv_color_t *color_p);
+    void disableTouchIRQ();
+    void enableTouchIRQ();
+
+
+};
+extern lvgl_7helperForS3 lvgl_7helper;
