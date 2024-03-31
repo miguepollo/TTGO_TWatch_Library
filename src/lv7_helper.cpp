dabf3485a1e7f0f7e2af32c4fc7c4d7e70db7f74
miguepollo <miguelgarciapel@protonmail.com>
2024-03-31 12:31:47 +0200

    Update lv7_helper.cpp

--- src/lv7_helper.cpp
+++ src/lv7_helper.cpp
@@ -1,45 +1,48 @@
 #include "lv7_helper.h"
 #include "lvgl.h"
-#include <Ticker.h>
-#if  defined(LILYGO_WATCH_LVGL)
-    Ticker *tickTicker = nullptr;
-#endif  /*LILYGO_WATCH_LVGL*/
+#include <config.h>
 
-#if (defined(LILYGO_WATCH_2020_V1) || defined(LILYGO_WATCH_2020_V2) ||  defined(LILYGO_WATCH_2020_S3) || defined(LILYGO_WATCH_2020_V3)|| defined(LILYGO_WATCH_2019_WITH_TOUCH)) &&  defined(LILYGO_WATCH_LVGL)
-    /*
-    Interrupt polling is only compatible with 2020-V1, 2020-V2, others are not currently adapted
-    */
+#if (defined(LILYGO_WATCH_2020_V1) || defined(LILYGO_WATCH_2020_V2) || defined(LILYGO_WATCH_2020_S3) || defined(LILYGO_WATCH_2020_V3) || defined(LILYGO_WATCH_2019_WITH_TOUCH)) && defined(LILYGO_WATCH_LVGL)
+/*
+Interrupt polling is only compatible with 2020-V1, 2020-V2, others are not currently adapted
+*/
 
-#endif  /*LILYGO_WATCH_2020_V1 & LILYGO_WATCH_2020_V2*/
+#endif /*LILYGO_WATCH_2020_V1 & LILYGO_WATCH_2020_V2*/
 
 #include "focaltech.h"
 #include <Arduino.h>
 #include "LilyGoLib.h"
 TFT_eSPI tft = TFT_eSPI();
 TFT_eSprite sprite = TFT_eSprite(&tft);
+
 #ifdef LILYGO_WATCH_LVGL
+    #include <Ticker.h>
 #endif
-#ifdef LILYGO_WATCH_LVGL
-#include <Ticker.h>
-#endif
-#if  defined(LILYGO_WATCH_LVGL) && defined(LILYGO_WATCH_HAS_DISPLAY)
+
+Ticker *tickTicker = nullptr;
+
+#if defined(LILYGO_WATCH_LVGL) && defined(LILYGO_WATCH_HAS_DISPLAY)
     lv_disp_drv_t disp_drv;
-#endif  /*LILYGO_WATCH_LVGL & LILYGO_WATCH_HAS_DISPLAY*/
-#if  defined(LILYGO_WATCH_LVGL) && defined(LILYGO_WATCH_HAS_DISPLAY)
+#endif /*LILYGO_WATCH_LVGL & LILYGO_WATCH_HAS_DISPLAY*/
+
+
+#if defined(LILYGO_WATCH_LVGL) && defined(LILYGO_WATCH_HAS_DISPLAY)
 
     static void disp_flush(lv_disp_drv_t *disp_drv, const lv_area_t *area, lv_color_t *color_p)
     {
-        uint32_t size = (area->x2 - area->x1 + 1) * (area->y2 - area->y1 + 1) ;
+        uint32_t size = (area->x2 - area->x1 + 1) * (area->y2 - area->y1 + 1);
+
         // Use DMA for transfer
-#if defined(ENABLE_LVGL_FLUSH_DMA)
-        tft.startWrite();
-        tft.setAddrWindow(area->x1, area->y1, (area->x2 - area->x1 + 1), (area->y2 - area->y1 + 1)); /* set the working window */
-        tft.pushPixelsDMA(( uint16_t *)color_p, size);
-        tft.endWrite();
-#else
-        tft.setAddrWindow(area->x1, area->y1, (area->x2 - area->x1 + 1), (area->y2 - area->y1 + 1)); /* set the working window */
-        tft.pushColors(( uint16_t *)color_p, size, false);
-#endif
+        #if defined(ENABLE_LVGL_FLUSH_DMA)
+            tft.startWrite();
+            tft.setAddrWindow(area->x1, area->y1, (area->x2 - area->x1 + 1), (area->y2 - area->y1 + 1)); /* set the working window */
+            tft.pushPixelsDMA((uint16_t *)color_p, size);
+            tft.endWrite();
+        #else
+            tft.setAddrWindow(area->x1, area->y1, (area->x2 - area->x1 + 1), (area->y2 - area->y1 + 1)); /* set the working window */
+            tft.pushColors((uint16_t *)color_p, size, false);
+        #endif
+
         lv_disp_flush_ready(disp_drv);
     }
 
@@ -51,152 +54,140 @@ TFT_eSprite sprite = TFT_eSprite(&tft);
         lv_disp_drv_update(lv_disp_get_default(), &disp_drv);
     }
 
-
-
-
-#ifdef  TWATCH_USE_PSRAM_ALLOC_LVGL
+    #ifdef TWATCH_USE_PSRAM_ALLOC_LVGL
     lv_color_t *buf1 = nullptr;
-#ifdef  TWATCH_LVGL_DOUBLE_BUFFER
+    #ifdef TWATCH_LVGL_DOUBLE_BUFFER
     lv_color_t *buf2 = nullptr;
-#endif
+    #endif
 #endif
 
-    bool lvgl_begin()
+bool lvgl_begin()
+{
+
+    lv_init();
+    lv_indev_drv_t indev_drv;
+    lv_disp_drv_init(&disp_drv);
+    static lv_disp_buf_t disp_buf;
+
+#ifdef TWATCH_USE_PSRAM_ALLOC_LVGL
+    if (psramFound()) {
+        buf1 = (lv_color_t *)ps_calloc(sizeof(lv_color_t), LVGL_BUFFER_SIZE);
+    } else {
+        buf1 = (lv_color_t *)calloc(sizeof(lv_color_t), LVGL_BUFFER_SIZE);
+    }
+    if (!buf1) {
+        log_e("buf1 alloc failed\n");
+        return false;
+    }
+#ifdef TWATCH_LVGL_DOUBLE_BUFFER
+    if (psramFound())
     {
-
-        lv_init();
-        lv_indev_drv_t indev_drv;
-        lv_disp_drv_init(&disp_drv);
-        static lv_disp_buf_t disp_buf;
-
-#ifdef  TWATCH_USE_PSRAM_ALLOC_LVGL
-        if (psramFound()) {
-            buf1 = (lv_color_t *)ps_calloc(sizeof(lv_color_t), LVGL_BUFFER_SIZE);
-        } else {
-            buf1 = (lv_color_t *)calloc(sizeof(lv_color_t), LVGL_BUFFER_SIZE);
-        }
-        if (!buf1) {
-            log_e("buf1 alloc failed\n");
-            return false;
-        }
-#ifdef  TWATCH_LVGL_DOUBLE_BUFFER
-        if (psramFound()) {
-            buf2 = (lv_color_t *)ps_calloc(sizeof(lv_color_t), LVGL_BUFFER_SIZE);
-        } else {
-            buf2 = (lv_color_t *)calloc(sizeof(lv_color_t), LVGL_BUFFER_SIZE);
-        }
-        if (!buf2) {
-            log_e("buf2 alloc failed\n");
-            free(buf1);
-            return false;
-        }
-#endif  /*TWATCH_LVGL_DOUBLE_BUFFER*/
+        buf2 = (lv_color_t *)ps_calloc(sizeof(lv_color_t), LVGL_BUFFER_SIZE);
+    }
+    else
+    {
+        buf2 = (lv_color_t *)calloc(sizeof(lv_color_t), LVGL_BUFFER_SIZE);
+    }
+    if (!buf2)
+    {
+        log_e("buf2 alloc failed\n");
+        free(buf1);
+        return false;
+    }
+#endif /*TWATCH_LVGL_DOUBLE_BUFFER*/
 #else
-        static lv_color_t buf1[LVGL_BUFFER_SIZE];
-#endif  /*TWATCH_LVGL_DOUBLE_BUFFER*/
+    static lv_color_t buf1[LVGL_BUFFER_SIZE];
+#endif /*TWATCH_LVGL_DOUBLE_BUFFER*/
 
-
-#ifdef  TWATCH_LVGL_DOUBLE_BUFFER
-        lv_disp_buf_init(&disp_buf, buf1, buf2, LVGL_BUFFER_SIZE);
+#ifdef TWATCH_LVGL_DOUBLE_BUFFER
+    lv_disp_buf_init(&disp_buf, buf1, buf2, LVGL_BUFFER_SIZE);
 #else
-        lv_disp_buf_init(&disp_buf, buf1, NULL, LVGL_BUFFER_SIZE);
+    lv_disp_buf_init(&disp_buf, buf1, NULL, LVGL_BUFFER_SIZE);
 #endif
 
+    disp_drv.hor_res = tft.width();
+    disp_drv.ver_res = tft.height();
+    disp_drv.flush_cb = disp_flush;
+    /*Set a display buffer*/
+    disp_drv.buffer = &disp_buf;
+    lv_disp_drv_register(&disp_drv);
 
-        disp_drv.hor_res = tft.width();
-        disp_drv.ver_res = tft.height();
-        disp_drv.flush_cb = disp_flush;
-        /*Set a display buffer*/
-        disp_drv.buffer = &disp_buf;
-        lv_disp_drv_register(&disp_drv);
-
-#if  defined(LILYGO_WATCH_HAS_TOUCH)
-        /*Register a touchpad input device*/
-        lv_indev_drv_init(&indev_drv);
-        indev_drv.type = LV_INDEV_TYPE_POINTER;
-        indev_drv.read_cb = touchpad_read;
-        lv_indev_drv_register(&indev_drv);
-#endif  /*LILYGO_WATCH_HAS_TOUCH*/
-
+#if defined(LILYGO_WATCH_HAS_TOUCH)
+    /*Register a touchpad input device*/
+    lv_indev_drv_init(&indev_drv);
+    indev_drv.type = LV_INDEV_TYPE_POINTER;
+    indev_drv.read_cb = touchpad_read;
+    lv_indev_drv_register(&indev_drv);
+#endif /*LILYGO_WATCH_HAS_TOUCH*/
 
 #if defined(LILYGO_WATCH_2020_S3)
 #define LILYGO_WATCH_LVGL_FS_SPIFFS
 #endif
 
 #ifdef LILYGO_WATCH_LVGL_FS
-#if  defined(LILYGO_WATCH_LVGL_FS_SPIFFS)
+    #if defined(LILYGO_WATCH_LVGL_FS_SPIFFS)
         SPIFFS.begin(true, "/fs");
-#else
-        //TODO:
-#endif  /*LILYGO_WATCH_LVGL_FS_SPIFFS*/
+    #else
+        // TODO:
+    #endif /*LILYGO_WATCH_LVGL_FS_SPIFFS*/
+#endif /*LILYGO_WATCH_LVGL_FS*/
 
-
-
-
-
-#endif  /*LILYGO_WATCH_LVGL_FS*/
     tickTicker = new Ticker();
-    void startLvglTick();
-    {
-        tickTicker->attach_ms(5, []() {
-            lv_tick_inc(5);
-        });
-    }
 
-        startLvglTick();
-        return true;
-    }
+    startLvglTick();
+    return true;
+}
 
+void startLvglTick() {
+    tickTicker->attach_ms(5, []()
+                            { lv_tick_inc(5); });
+}
 
+void stopLvglTick()
+{
+    tickTicker->detach();
+}
 
-    void stopLvglTick()
-    {
-        tickTicker->detach();
-    }
-
-#endif  /*LILYGO_WATCH_LVGL*/
-#if (defined(LILYGO_WATCH_2020_V1) || defined(LILYGO_WATCH_2020_V2) || defined(LILYGO_WATCH_2020_V3)|| defined(LILYGO_WATCH_2020_S3)||defined(LILYGO_WATCH_2019_WITH_TOUCH)) &&  defined(LILYGO_WATCH_LVGL)
-    void disableTouchIRQ()
-    {
-        detachInterrupt(BOARD_TOUCH_INT);
-    }
-
-
-
+#endif /*LILYGO_WATCH_LVGL*/
+#if (defined(LILYGO_WATCH_2020_V1) || defined(LILYGO_WATCH_2020_V2) || defined(LILYGO_WATCH_2020_V3) || defined(LILYGO_WATCH_2020_S3) || defined(LILYGO_WATCH_2019_WITH_TOUCH)) && defined(LILYGO_WATCH_LVGL)
+void disableTouchIRQ()
+{
+    detachInterrupt(BOARD_TOUCH_INT);
+}
 
 #endif
 
-
 #if defined(LILYGO_WATCH_LVGL) && defined(LILYGO_WATCH_HAS_DISPLAY)
 
-
 #if defined(LILYGO_WATCH_LVGL) && defined(LILYGO_WATCH_HAS_TOUCH)
-    static bool touchpad_read(lv_indev_drv_t *indev_drv, lv_indev_data_t *data)
-    {
+static bool touchpad_read(lv_indev_drv_t *indev_drv, lv_indev_data_t *data)
+{
 
-#if (defined(LILYGO_WATCH_2020_V1) || defined(LILYGO_WATCH_2020_V2) || defined(LILYGO_WATCH_2020_V3)|| defined(LILYGO_WATCH_2020_S3)|| defined(LILYGO_WATCH_2019_WITH_TOUCH)) &&  defined(LILYGO_WATCH_LVGL)
-        /*
-            Interrupt polling is only compatible with 2020-V1, 2020-V2,2019, others are not currently adapted
-        */
-        static int16_t x, y;
-        if (xEventGroupGetBits(_ttgo->_tpEvent) & TOUCH_IRQ_BIT) {
-            xEventGroupClearBits(_ttgo->_tpEvent, TOUCH_IRQ_BIT);
-            data->state = watch.getTouch(x, y) ?  LV_INDEV_STATE_PR : LV_INDEV_STATE_REL;
-            data->point.x = x;
-            data->point.y = y;
-        } else {
-            data->state = LV_INDEV_STATE_REL;
-            data->point.x = x;
-            data->point.y = y;
-        }
+#if (defined(LILYGO_WATCH_2020_V1) || defined(LILYGO_WATCH_2020_V2) || defined(LILYGO_WATCH_2020_V3) || defined(LILYGO_WATCH_2020_S3) || defined(LILYGO_WATCH_2019_WITH_TOUCH)) && defined(LILYGO_WATCH_LVGL)
+    /*
+        Interrupt polling is only compatible with 2020-V1, 2020-V2,2019, others are not currently adapted
+    */
+    static int16_t x, y;
+    if (xEventGroupGetBits(_ttgo->_tpEvent) & TOUCH_IRQ_BIT)
+    {
+        xEventGroupClearBits(_ttgo->_tpEvent, TOUCH_IRQ_BIT);
+        data->state = watch.getTouch(x, y) ? LV_INDEV_STATE_PR : LV_INDEV_STATE_REL;
+        data->point.x = x;
+        data->point.y = y;
+    }
+    else
+    {
+        data->state = LV_INDEV_STATE_REL;
+        data->point.x = x;
+        data->point.y = y;
+    }
 #else
-        data->state = watch.getTouch(data->point.x, data->point.y) ?  LV_INDEV_STATE_PR : LV_INDEV_STATE_REL;
+    data->state = watch.getTouch(data->point.x, data->point.y) ? LV_INDEV_STATE_PR : LV_INDEV_STATE_REL;
 #endif /*LILYGO_WATCH_2020_V1 & LILYGO_WATCH_2020_V2*/
 
-        return false; /*Return false because no more to be read*/
-    }
+    return false; /*Return false because no more to be read*/
+}
 
 #endif /*LILYGO_WATCH_LVGL , LILYGO_WATCH_HAS_TOUCH*/
 
 #endif /*LILYGO_WATCH_LVGL , LILYGO_WATCH_HAS_DISPLAY*/
-
