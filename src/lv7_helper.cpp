725214589c9c55c3026cc1f4476469feb795457c
miguepollo <127879645+miguepollo@users.noreply.github.com>
2025-03-03 10:41:36 +0100

    Update lv7_helper.cpp

--- src/lv7_helper.cpp
+++ src/lv7_helper.cpp
@@ -1,6 +1,8 @@
 #include "lv7_helper.h"
 #include "lvgl.h"
 #include <config.h>
+#include "config.h"  // Asegúrate de incluir los archivos necesarios
+extern TTGOClass watch;  // Declara watch como variable externa
 
 #if (defined(LILYGO_WATCH_2020_V1) || defined(LILYGO_WATCH_2020_V2) || defined(LILYGO_WATCH_2020_S3) || defined(LILYGO_WATCH_2020_V3) || defined(LILYGO_WATCH_2019_WITH_TOUCH)) && defined(LILYGO_WATCH_LVGL)
 /*
